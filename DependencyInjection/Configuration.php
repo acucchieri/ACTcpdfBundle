f9949b53aecba6331d1b723a140aa891714975dd
acucchieri <acucchieri@users.noreply.github.com>
2019-03-27 22:56:50 +0100

    change github url

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the ACTcpdfBundle package.
  *
- * (c) acucchieri <http://acucchieri.github.com/>
+ * (c) acucchieri <https://github.com/acucchieri>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
