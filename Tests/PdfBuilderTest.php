a7677fef29c3d6085bc156a7b4c45c1494a12251
acucchieri <acucchieri@users.noreply.github.com>
2019-03-27 23:03:30 +0100

    CS

--- Tests/PdfBuilderTest.php
+++ Tests/PdfBuilderTest.php
@@ -1,10 +1,18 @@
 <?php
 
+/*
+ * This file is part of the ACTcpdfBundle package.
+ *
+ * (c) acucchieri <https://github.com/acucchieri>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AC\TcpdfBundle\Tests;
 
 use AC\TcpdfBundle\Pdf\PdfBuilder;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class PdfBuilderTest extends TestCase
 {
